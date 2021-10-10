43404ad42afd83be69fc00c4d609fd4185973429
Rajput Shivam <66658735+shivamrajput177@users.noreply.github.com>
2021-10-10 09:57:12 +0530

    bug fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,23 +8,21 @@ import { Component } from "react";
 class App extends Component {
   render(){
   return (
-<<<<<<< HEAD
+    <>
 
   <div className="App">  
     
   <Routing/>
   
    </div>
-  )
-  }
-=======
-    <div className="App">
+   <div className="App">
       <header className="App-header">
         <Map/>
       </header>
     </div>
-  );
->>>>>>> c52f9ccafd355e64710b1e0e5528554dfdea346c
+   </>
+  )
+  }
 }
 
 
